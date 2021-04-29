b670696e5b871b71d22ed27b192f56ba35612cd2
Stas_PC <stazzzdzr@gmail.com>
2021-04-29 22:13:56 +0300

    lesson 3 finish

---
+++ lesson_2/js/script.js
@@ -0,0 +1,16 @@
+"use strick"
+
+// alert("hello");
+
+// const result = confirm('Are you here?');
+// alert(result);
+
+const answers = [];
+
+// answers[0] = prompt('Как ваше имя ', '');
+// answers[1] = prompt('Как ваша фамилия? ', '');
+// answers[2] = prompt('Сколько вам лет? ', '');
+// alert(answers[0] + " " + answers[1] + " " + answers[2]);
+
+
+console.log(typeof(answers));
