3b0a0ecca05199a093dd0fabb9820aa57664f56d
Stas <stazzzdzr@gmail.com>
2021-06-08 19:34:06 +0300

    Revert "testing func"
    This reverts commit 03182df78c49cee7a832fedec127d18e2f1e04cc.

--- test/this/js/script.js
+++ test/this/js/script.js
@@ -1,23 +1,40 @@
-"use strick"
-let b = 1;
-let fer = (function (){
-	
-	let a = 1;
+'use strict';
 
-	return {
-		say: function() {
-			console.log(a);
-			a++;
-			if (a > 15) {
-				clearInterval(id);
-				console.log('finish')
-			}
-		}
-	};
-}());
+class Car {
+	constructor (color, engin){
+		this.color = color;
+		this.engin = engin;
+	}
+	start (){
+		console.log(`${this.engin}: rrRRRrrrRR`);
+	}
+}
 
-let id = setInterval(() => {
-	fer.say();
-	
-}, 300);
+const div = document.createElement('div'),
+		body = document.querySelector('body');
+		body.append(div);
 
+
+		div.style.cssText =`
+		width: 100%;
+		height: 300px;
+		background-color: pink;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		`;
+
+		div.innerHTML = `
+		<input type="text">
+		<input type="text">
+		<input type="text">
+		`;
+
+const inputs = document.querySelectorAll('input');
+inputs.forEach((e) => {
+	e.style.cssText = `
+		display: block;
+		height: 40px
+		`;
+})
+		
